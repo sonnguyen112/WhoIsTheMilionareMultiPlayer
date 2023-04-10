c0f331c917d16d9843f9ca8a15f664bf74940c14
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2023-04-10 14:40:18 +0700

    add sqlite

--- ProjectSocket/src/main/java/Server/Server.java
+++ ProjectSocket/src/main/java/Server/Server.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
+import java.sql.*;
 
 public class Server {
     private final int NUM_PLAYER = 4;
@@ -34,8 +35,8 @@ public class Server {
     Connection con = null;
 
     public Server() throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.jdbc.Driver");  
-        con=DriverManager.getConnection("jdbc:mysql://localhost:3306/internet1db","root","Khoi2921432");  
+        Class.forName("org.sqlite.JDBC");  
+        con=DriverManager.getConnection("jdbc:sqlite:test.db");  
         //here sonoo is database name, root is username and password  
         stmt= con.createStatement();  
     }
