fc0e4a935e36d0a6631a693aa799135b8fa76155
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2023-04-12 02:04:08 +0700

    u

--- ProjectSocket/src/main/java/Server/Server.java
+++ ProjectSocket/src/main/java/Server/Server.java
@@ -22,7 +22,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class Server {
-    private final int NUM_PLAYER = 2;
+    private final int NUM_PLAYER = 4;
     private Selector selector = null;
     private ArrayList<SocketChannel> clients = new ArrayList<>();
     private ByteBuffer bufferRead = ByteBuffer.allocate(1024);
@@ -35,42 +35,42 @@ public class Server {
     private ArrayList<Integer> trueAnswer = new ArrayList<>();
     Statement stmt = null;
     Connection con = null;
+    ResultSet rs_question = null;
     int curPlayerIndex = 0;
 
-    public Server() throws ClassNotFoundException, SQLException{
-        Class.forName("org.sqlite.JDBC");  
-        con=DriverManager.getConnection("jdbc:sqlite:test.db");  
-        //here sonoo is database name, root is username and password  
-        stmt= con.createStatement();  
+    public Server() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        con = DriverManager.getConnection("jdbc:sqlite:test.db");
+        // here sonoo is database name, root is username and password
+        stmt = con.createStatement();
+        rs_question = stmt.executeQuery("select * from questions");
+
     }
     // Integer indexOption=0;
-    // ResultSet rs=stmt.executeQuery("select * from options WHERE isTrue =" + true);  
-    // while(rs.next())  
+    // ResultSet rs=stmt.executeQuery("select * from options WHERE isTrue =" +
+    // true);
+    // while(rs.next())
     // {
-    
-    //     indexOption = rs.getInt(1);
-    //     trueAnswer.add(indexOption);
-    // }  
+
+    // indexOption = rs.getInt(1);
+    // trueAnswer.add(indexOption);
+    // }
 
     public void run() throws ClassNotFoundException, SQLException {
         try {
             selector = Selector.open();
             // We have to set connection host,port and
             // non-blocking mode
-            ServerSocketChannel serverSocketChannel
-                    = ServerSocketChannel.open();
-            ServerSocket serverSocket
-                    = serverSocketChannel.socket();
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            ServerSocket serverSocket = serverSocketChannel.socket();
             serverSocket.bind(
                     new InetSocketAddress("localhost", 8089));
             serverSocketChannel.configureBlocking(false);
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
             while (true) {
                 selector.select();
-                Set<SelectionKey> selectedKeys
-                        = selector.selectedKeys();
-                Iterator<SelectionKey> i
-                        = selectedKeys.iterator();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> i = selectedKeys.iterator();
 
                 while (i.hasNext()) {
                     SelectionKey key = i.next();
@@ -93,7 +93,7 @@ public class Server {
     }
 
     private void handleAccept(ServerSocketChannel mySocket,
-                              SelectionKey key) throws IOException {
+            SelectionKey key) throws IOException {
 
         System.out.println("Connection Accepted..");
 
@@ -127,82 +127,87 @@ public class Server {
     }
 
     private void sendData(String data, SocketChannel client) throws IOException {
-        bufferWrite.limit(1024);
         bufferWrite.put(data.getBytes());
         bufferWrite.flip();
         client.write(bufferWrite);
-        bufferWrite.clear();
+        bufferWrite = ByteBuffer.allocate(1024);
     }
-    
-    private  String getQuestionAction() throws ClassNotFoundException, SQLException{
-        String question = ""; 
+
+    private String getQuestionAction() throws ClassNotFoundException, SQLException {
+        String question = "";
         int questionId = 0;
-        Integer indexOption=0;
-        ResultSet rs=stmt.executeQuery("select * from questions");  
-        while(rs.next())  
-        {
-            question = rs.getString(2);
-            questionId = rs.getInt(1); 
-            indexOption = rs.getInt(3);
+        Integer indexOption = 0;
+        while (rs_question.next()) {
+            question = rs_question.getString(2);
+            questionId = rs_question.getInt(1);
+            indexOption = rs_question.getInt(3);
             trueAnswer.add(indexOption);
             questionList.add(question);
             questionListId.add(questionId);
-        }  
-        String result = questionList.remove(0);
-        
+        }
+        String result = "Run out of question";
+        if (!questionList.isEmpty()) {
+            result = questionList.remove(0);
+        }
+
         return result;
-        
+
     }
 
-    private Boolean getCheckedCorr(Object object){
-        if (trueAnswer.contains(object))
-        {
+    private Boolean getCheckedCorr(Object object) {
+        if (trueAnswer.contains(object)) {
             return true;
         }
         return false;
     }
 
+    private Boolean getCheckedIwin() {
 
-    private Boolean getCheckedIwin(){
-        if((questionList.isEmpty()) || (namePlayers.size()==1))
-        {
+        if ((questionList.isEmpty()) || (namePlayers.size() == 1)) {
             return true;
         }
         return false;
     }
 
-    private ArrayList<String> getListName(boolean corr,Object clientIndex){
-        ArrayList<String> ListName = new ArrayList<>();
-        for (int i = 0; i < namePlayers.size(); i++) {
-            ListName.add(namePlayers.get(i));
+    private ArrayList<String> getListName(boolean corr, SocketChannel client) throws IOException {
+
+        if (corr == false) {
+            namePlayers.remove(namePlayers.get(clients.indexOf(client)));
+            clients.remove(client);
+
         }
-        if(!corr)
-        {
-            ListName.remove(clientIndex);
-        }
-        return ListName;
+        System.out.println(namePlayers);
+        return namePlayers;
     }
-    private Integer getIndexQuestion(){
-        Integer CurrentIndex =  questionListId.get(0);
+
+    private Integer getIndexQuestion() {
+        Integer CurrentIndex = 0;
+        if (!questionListId.isEmpty()) {
+            CurrentIndex = questionListId.get(0);
+        }
         return CurrentIndex;
     }
-    private  HashMap<Integer,String> getListOptions() throws ClassNotFoundException, SQLException{
-        HashMap<Integer,String> ListOptions = new HashMap<>();
+
+    private HashMap<Integer, String> getListOptions() throws ClassNotFoundException, SQLException {
+        HashMap<Integer, String> ListOptions = new HashMap<>();
         String option = "";
         Integer indexOption = 0;
-        checkedQuestionListId.add(questionListId.get(0));
-        ResultSet rs=stmt.executeQuery("select * from options WHERE QuestionID="+questionListId.remove(0));  
-        while(rs.next())  
-        {
-            option = rs.getString(2);
-            indexOption = rs.getInt(1);
-            ListOptions.put(indexOption,rs.getString(2));
-            System.out.println(indexOption+"  "+option+"  "+rs.getInt(3));
-        }  
+        if (!questionListId.isEmpty()) {
+            checkedQuestionListId.add(questionListId.get(0));
+            ResultSet rs = stmt.executeQuery("select * from options WHERE QuestionID=" + questionListId.remove(0));
+            while (rs.next()) {
+                option = rs.getString(2);
+                indexOption = rs.getInt(1);
+                ListOptions.put(indexOption, rs.getString(2));
+                System.out.println(indexOption + "  " + option + "  " + rs.getInt(3));
+            }
+        }
+
         return ListOptions;
     }
 
-    private void processData(String data, SocketChannel client) throws IOException, ClassNotFoundException, SQLException {
+    private void processData(String data, SocketChannel client)
+            throws IOException, ClassNotFoundException, SQLException {
         if (data.equalsIgnoreCase(
                 "quit")) {
             // for (int i = 0; i < clients.size(); i++) {
@@ -211,6 +216,13 @@ public class Server {
             return;
         }
 
+        if (data.equalsIgnoreCase(
+                "reset")) {
+            for (int i = 0; i < clients.size(); i++) {
+                if (clients.get(i) != client) sendData("reset", clients.get(i));
+            }
+            System.out.println("Connection reset...");
+        }
 
         HashMap<String, Object> jsonData = objectMapper.readValue(data, HashMap.class);
         if (jsonData.get("event").equals("joinRoom")) {
@@ -243,54 +255,75 @@ public class Server {
                 return;
             }
         }
-        if (jsonData.get("event").equals("getQuestion")){
+        if (jsonData.get("event").equals("getQuestion")) {
             HashMap<String, Object> response = new HashMap<>();
-            response.put("event","getQuestion");
             String question = getQuestionAction();
-            response.put("question", question);
-            Integer questionId = getIndexQuestion();
-            response.put("questionId", questionId);
-            HashMap<Integer,String> ListOptions = getListOptions();
-            response.put("options", ListOptions);
-            response.put("name_player", namePlayers.get(curPlayerIndex ));
-            curPlayerIndex = (curPlayerIndex + 1) % NUM_PLAYER;
-            String jsonResponse = objectMapper.writeValueAsString(response);
-            for (int i = 0; i < clients.size(); i++) sendData(jsonResponse, clients.get(i));
+            if (!question.equals("Run out of question")) {
+                response.put("event", "getQuestion");
+                response.put("question", question);
+                Integer questionId = getIndexQuestion();
+                response.put("questionId", questionId);
+                HashMap<Integer, String> ListOptions = getListOptions();
+                response.put("options", ListOptions);
+                curPlayerIndex = curPlayerIndex % namePlayers.size();
+                response.put("name_player", namePlayers.get(curPlayerIndex));
+                curPlayerIndex = (curPlayerIndex + 1) % namePlayers.size();
+                String jsonResponse = objectMapper.writeValueAsString(response);
+                for (int i = 0; i < clients.size(); i++)
+                    sendData(jsonResponse, clients.get(i));
+            } else {
+                return;
+            }
         }
-        if (jsonData.get("event").equals("answer")){
+        if (jsonData.get("event").equals("answer")) {
             HashMap<String, Object> response = new HashMap<>();
             if (checkedQuestionListId.contains(jsonData.get("question"))) {
                 response.put("event", "answer");
-                Boolean corr = getCheckedCorr(Integer.parseInt((String) jsonData.get("answer")));
+                String temptAnswer = (String) jsonData.get("answer");
+                Boolean corr = false;
+                if (temptAnswer.equals("-1")) {
+                    ResultSet rs = stmt
+                            .executeQuery("select * from questions WHERE QuestionID=" + jsonData.get("question"));
+                    String question = "";
+                    int questionId = 0;
+                    Integer indexOption = 0;
+                    while (rs.next()) {
+
+                        question = rs_question.getString(2);
+                        questionId = rs_question.getInt(1);
+                        indexOption = rs_question.getInt(3);
+                        trueAnswer.add(indexOption);
+                        questionList.add(question);
+                        questionListId.add(questionId);
+                    }
+                    corr = true;
+                } else if (temptAnswer.equals("-2")) {
+                    corr = false;
+                } else {
+                    corr = getCheckedCorr(Integer.parseInt(temptAnswer));
+                }
                 response.put("corr", corr);
+                System.out.println("remain player: " + namePlayers.size());
                 Boolean iwin = getCheckedIwin();
                 response.put("iwin", iwin);
-                ArrayList<String> ListName = getListName(corr,client);
+                ArrayList<String> ListName = getListName(corr, client);
                 response.put("mess", ListName);
                 String jsonResponse = objectMapper.writeValueAsString(response);
                 sendData(jsonResponse, client);
                 System.out.println("send: " + jsonResponse);
-                
-                // response = new HashMap<>();
-                // response.put("event", "update");
-                // response.put("status", "success");
-                // response.put("mess", ListName);
-                // jsonResponse = "";
-                // jsonResponse = objectMapper.writeValueAsString(response);
-                // for (int i = 0; i < clients.size(); i++) {
-                //     sendData(jsonResponse, clients.get(i));
-                // }
+
+                response = new HashMap<>();
+                response.put("event", "update");
+                response.put("status", "success");
+                response.put("mess", ListName);
+                jsonResponse = "";
+                jsonResponse = objectMapper.writeValueAsString(response);
+                System.out.println("send: " + jsonResponse);
+                for (int i = 0; i < clients.size(); i++) {
+                    sendData(jsonResponse, clients.get(i));
+                }
                 return;
-        }
-        if (data.equalsIgnoreCase(
-                "quit")) {
-            for (int i = 0; i < clients.size(); i++) {
-                clients.get(i).close();
             }
-            System.out.println("Connection closed...");
         }
     }
 }
-}
-
- 
